eb78cb28235fe15333b9c90107ad3790c259efd1
gaips1 <50048569+gaips1@users.noreply.github.com>
2026-07-05 18:13:17 +0300

    feat: add marriages command to display top marriages by duration

--- src/modules/marriages/mod.rs
+++ src/modules/marriages/mod.rs
@@ -2,6 +2,7 @@ use crate::{types::*};
 use poise::serenity_prelude as serenity;
 
 mod marry;
+mod marriages;
 pub mod my_marriage;
 
 pub async fn handle_marriages_buttons(
@@ -9,7 +10,9 @@ pub async fn handle_marriages_buttons(
     interaction: &serenity::ComponentInteraction,
     data: &Data
 ) -> Result<(), Error> {
-    if interaction.data.custom_id.as_str().starts_with("marriage:divorce") {
+    let custom_id = interaction.data.custom_id.as_str();
+
+    if custom_id.starts_with("marriage:divorce") {
         my_marriage::handle_divorce_button(ctx, interaction, data).await?;
     }
     Ok(())
@@ -18,6 +21,7 @@ pub async fn handle_marriages_buttons(
 pub fn commands() -> Vec<poise::Command<Data, Error>> {
     vec![
         marry::marry(),
-        my_marriage::my_marriage()
+        my_marriage::my_marriage(),
+        marriages::marriages()
     ]
 }
