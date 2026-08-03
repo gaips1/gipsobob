74099740c56eb072246271f650d70df1f2d43885
gaips1 <50048569+gaips1@users.noreply.github.com>
2026-08-03 21:07:50 +0300

    feat: enhance traits handling with user balance checks and update trait display messages

--- src/modules/traits/spin.rs
+++ src/modules/traits/spin.rs
@@ -1,4 +1,7 @@
-use crate::{modules::traits::{UserTrait, main_menu::format_user_trait}, types::*};
+use rand::seq::{IndexedRandom as _};
+use rust_decimal::Decimal;
+
+use crate::{modules::{dialogues, traits::{UserTrait, main_menu::format_user_trait}}, types::*};
 
 pub async fn handle_traits_spin_button(
     ctx: &serenity::Context,
@@ -6,31 +9,32 @@ pub async fn handle_traits_spin_button(
     data: &Data,
 ) -> Result<(), Error> {
     let all_traits = super::get_traits();
-
-    let rows: Vec<(i16, Option<String>, Option<i16>)> = sqlx::query_as(
-        "SELECT \
-            u.unlocked_traits_slots, \
-            ut.trait_id, \
-            ut.slot_index \
-        FROM users u \
-        LEFT JOIN user_traits ut ON u.id = ut.user_id \
-        WHERE u.id = $1 \
-        ORDER BY ut.slot_index ASC",
-    )
-    .bind(press.user.id.get() as i64)
-    .fetch_all(&data.pool)
-    .await?;
-
-    let user_unlocked_slots = rows.first().unwrap().0;
-    let user_traits: Vec<UserTrait> = rows
-        .into_iter()
-        .filter_map(|r| Some(UserTrait { trait_id: r.1?, slot_index: r.2? }))
-        .collect();
+    let user_id = press.user.id.get() as i64;
 
     let Some(slot) = press.data.custom_id.strip_prefix("traits:spin:") else {
+        let rows: Vec<(i16, Option<String>, Option<i16>)> = sqlx::query_as(
+            "SELECT \
+                u.unlocked_traits_slots, \
+                ut.trait_id, \
+                ut.slot_index \
+            FROM users u \
+            LEFT JOIN user_traits ut ON u.id = ut.user_id \
+            WHERE u.id = $1 \
+            ORDER BY ut.slot_index ASC",
+        )
+        .bind(user_id)
+        .fetch_all(&data.pool)
+        .await?;
+
+        let user_unlocked_slots = rows.first().unwrap().0;
+        let user_traits: Vec<UserTrait> = rows
+            .into_iter()
+            .filter_map(|r| Some(UserTrait { trait_id: r.1?, slot_index: r.2? }))
+            .collect();
+
         let buttons: Vec<_> = (0..user_unlocked_slots)
             .map(|slot| {
-                let empty = UserTrait { trait_id: "empty".into(), slot_index: slot };
+                let empty = UserTrait { trait_id: "!empty".into(), slot_index: slot };
                 let t = user_traits.iter().find(|t| t.slot_index == slot).unwrap_or(&empty);
 
                 serenity::CreateButton::new(format!("traits:spin:{slot}"))
@@ -64,7 +68,16 @@ pub async fn handle_traits_spin_button(
     };
     let slot: u16 = slot.parse()?;
 
-    if slot > user_unlocked_slots as u16 {
+    let unlocked_slots: i16 = sqlx::query_scalar(
+        "SELECT unlocked_traits_slots \
+        FROM users \
+        WHERE id = $1"
+    )
+    .bind(user_id)
+    .fetch_one(&data.pool)
+    .await?;
+
+    if slot + 1 > unlocked_slots as u16 {
         crate::create_edit_response!(
             ctx,
             press,
@@ -76,7 +89,90 @@ pub async fn handle_traits_spin_button(
         return Ok(());
     }
 
-    
+    let mut tx = data.pool.begin().await?;
+
+    let user_balance: Decimal =
+        sqlx::query_scalar("SELECT balance FROM sbp_users WHERE id = $1 FOR UPDATE")
+            .bind(press.user.id.get() as i64)
+            .fetch_one(&mut *tx)
+            .await?;
+
+    if user_balance < Decimal::from(500) {
+        tx.rollback().await?;
+
+        let dialogue = dialogues::get_dialogue("traits:not_enough_money").unwrap();
+
+        let embed = serenity::CreateEmbed::new()
+            .title("Мутации")
+            .description(format!(
+                "{}\n\nВыберите слот, в который хотите вколоть мутацию.\n**Выбранный слот будет перезаписан**",
+                dialogue.content
+            ))
+            .colour(serenity::colours::branding::RED);
+
+        crate::create_edit_response!(
+            ctx,
+            press,
+            serenity::CreateInteractionResponseMessage::new()
+                .embed(embed)
+        );
+        return Ok(());
+    }
+
+    sqlx::query("UPDATE sbp_users SET balance = balance - 500 WHERE id = $1")
+        .bind(user_id)
+        .execute(&mut *tx)
+        .await?;
+
+    let all_traits: Vec<_> = all_traits.iter().filter(|t| !t.0.starts_with("!")).collect();
+    let random_trait = {
+        let mut rng = rand::rng();
+        all_traits.choose(&mut rng).unwrap()
+    };
+
+    let is_inserted: bool = sqlx::query_scalar(
+        "INSERT INTO user_traits (user_id, slot_index, trait_id) \
+        VALUES ($1, $2, $3) \
+        ON CONFLICT (user_id, slot_index) \
+        DO UPDATE SET trait_id = EXCLUDED.trait_id \
+        RETURNING (xmax = 0) AS is_created;"
+    )
+        .bind(user_id)
+        .bind(slot as i16)
+        .bind(random_trait.0)
+        .fetch_one(&mut *tx)
+        .await?;
+
+    tx.commit().await?;
+
+    let dialogue = dialogues::get_dialogue_with_vars(
+        if is_inserted { "traits:spin:empty" } else { "traits:spin:replace" },
+        &[("trait_name", random_trait.1)]
+    ).unwrap();
+
+    let embed = serenity::CreateEmbed::new()
+        .title("Мутации")
+        .description(format!(
+            "💉 Доктор Хальцер набирает в шприц зелёную жидкость из 3-литровой банки...\n\n{} 🎉",
+            dialogue.content
+        ))
+        .colour(serenity::colours::branding::GREEN);
+
+    crate::create_edit_response!(
+        ctx,
+        press,
+        serenity::CreateInteractionResponseMessage::new()
+            .embed(embed)
+            .components(vec![
+                serenity::CreateActionRow::Buttons(
+                    vec![
+                        serenity::CreateButton::new("traits:mm")
+                            .label("В главное меню")
+                            .style(serenity::ButtonStyle::Secondary)
+                    ]
+                )
+            ])
+    );
 
     Ok(())
 }
